00a976a8d8c6ea5fb3a3f71ad4399a1ba18d3a0e
TegHocine <arcou98@hotmail.com>
2022-08-24 11:41:56 +0100

    style change for display, preview image functionalite in postcomp

--- src/components/shareComp/ShareComp.tsx
+++ src/components/shareComp/ShareComp.tsx
@@ -1,15 +1,30 @@
+import { useState, useRef } from 'react'
 import {
   AiOutlinePicture,
   AiOutlinePlayCircle,
   AiOutlineEnvironment,
   AiOutlineCalendar
 } from 'react-icons/ai'
+import { MdDeleteForever } from 'react-icons/md'
 import Avatar from '../avatar/Avatar'
 import Button from '../button/Button'
 
 import './ShareComp.scss'
 
 const ShareComp = (): JSX.Element => {
+  const [image, setImage] = useState<string>('')
+  const imageRef = useRef<HTMLInputElement>(null)
+
+  const onImageChange = (event: any) => {
+    if (event.target.files && event.target.files[0]) {
+      let img = event.target.files[0]
+      setImage(URL.createObjectURL(img))
+    }
+  }
+
+  const deleteImage = () => {
+    setImage('')
+  }
   return (
     <div className='shareComp'>
       <Avatar image='/src/assets/profileImg.jpg' />
@@ -20,7 +35,7 @@ const ShareComp = (): JSX.Element => {
             title='Photo'
             icon={<AiOutlinePicture />}
             alter
-            onclick={() => console.log('hi')}
+            onclick={() => imageRef?.current?.click()}
           />
           <Button
             title='Video'
@@ -42,7 +57,23 @@ const ShareComp = (): JSX.Element => {
           />
 
           <Button title='Share' onclick={() => console.log('hi')} />
+          <div style={{ display: 'none' }}>
+            <input
+              type='file'
+              name='myImage'
+              ref={imageRef}
+              onChange={(e) => onImageChange(e)}
+            />
+          </div>
         </div>
+        {image ? (
+          <div className='previewImage'>
+            <img src={image} alt='' />
+            <div className='delete-image' onClick={deleteImage}>
+              <MdDeleteForever />
+            </div>
+          </div>
+        ) : null}
       </div>
     </div>
   )
